296c6b387878adf84d40e97e1f8b0637fdc21f84
yyhhenry <yyhhenry@foxmail.com>
2024-04-11 19:22:14 +0800

    Remove xmake

--- main.cpp
+++ main.cpp
@@ -114,43 +114,61 @@ struct Lexer {
                 pos++;
             }
             if (pos < src.size() && isalpha(src[pos])) {
-                cerr << lett << src[pos] << " is not a valid digit";
-                throw "invalid digit";
+                cerr << lett << src[pos] << " invalid integer literal" << endl;
+                throw "invalid integer literal";
             }
             return Token{IntLiteral, start, lett};
         } else if (src[pos] == ':' && pos < src.size() + 1 && src[pos + 1] == '=') {
+            pos += 2;
             return Token{Assign, start, ":="};
         } else if (src[pos] == '<' && pos < src.size() + 1 && src[pos + 1] == '=') {
+            pos += 2;
             return Token{Le, start, "<="};
         } else if (src[pos] == '>' && pos < src.size() + 1 && src[pos + 1] == '=') {
+            pos += 2;
             return Token{Ge, start, ">="};
         } else if (src[pos] == '<' && pos < src.size() + 1 && src[pos + 1] == '>') {
+            pos += 2;
             return Token{Ne, start, "<>"};
+        } else if (src[pos] == ':') {
+            pos++;
+            return Token{Colon, start, ":"};
         } else if (src[pos] == ',') {
+            pos++;
             return Token{Comma, start, ","};
         } else if (src[pos] == ';') {
+            pos++;
             return Token{SemiColon, start, ";"};
         } else if (src[pos] == '+') {
+            pos++;
             return Token{Add, start, "+"};
         } else if (src[pos] == '-') {
+            pos++;
             return Token{Sub, start, "-"};
         } else if (src[pos] == '*') {
+            pos++;
             return Token{Mul, start, "*"};
         } else if (src[pos] == '/') {
+            pos++;
             return Token{Div, start, "/"};
         } else if (src[pos] == '<') {
+            pos++;
             return Token{Lt, start, "<"};
         } else if (src[pos] == '>') {
+            pos++;
             return Token{Gt, start, ">"};
         } else if (src[pos] == '=') {
+            pos++;
             return Token{Eq, start, "="};
         } else if (src[pos] == '(') {
+            pos++;
             return Token{LParen, start, "("};
         } else if (src[pos] == ')') {
+            pos++;
             return Token{RParen, start, ")"};
         } else {
-            cerr << src[pos] << " unexpected character";
-            throw "unexpected character";
+            cerr << src[pos] << " Unexpected character" << endl;
+            throw "Unexpected character";
         }
         // unimplemented
     }
@@ -163,8 +181,8 @@ struct PreGrammar {
 
     void parse_var_block() {
         if (!(pos < tokens.size() && tokens[pos].type == Var)) {
-            cerr << "expect var";
-            throw "missing var";
+            cerr << tokens[pos].content << " except a Var" << endl;
+            throw "missing Var";
         }
         pos++;
         int flag;
@@ -174,7 +192,7 @@ struct PreGrammar {
             while (pos < tokens.size()) {
 
                 if (tokens[pos].type != Ident) {
-                    cerr << tokens[pos].content << " not a valid Ident";
+                    cerr << tokens[pos].content << " not a valid Ident" << endl;
                     throw "invalid Ident";
                 } else {
 
@@ -187,8 +205,8 @@ struct PreGrammar {
                     pos++;
                     break;
                 } else {
-                    cerr << tokens[pos].content << " except a ,or:";
-                    throw "missing Comma or colon";
+                    cerr << tokens[pos].content << " except a ,or:" << endl;
+                    throw "missing , or :";
                 }
             }
             if (pos < tokens.size() && (tokens[pos].type == Integer || tokens[pos].type == Bool || tokens[pos].type == Real || tokens[pos].type == Longint)) {
@@ -196,24 +214,25 @@ struct PreGrammar {
                 if (pos < tokens.size() && tokens[pos].type == SemiColon) {
                     pos++;
                 } else {
-                    cerr << tokens[pos].content << " except a ;";
-                    throw "missing SemiColon";
+                    cerr << tokens[pos].content << " except a ;" << endl;
+                    throw "missing ;";
                 }
             } else {
 
-                cerr << tokens[pos].content << " except a valid type";
+                cerr << tokens[pos].content << " except a valid type" << endl;
                 throw "invalid type";
             }
 
             if (pos < tokens.size() && tokens[pos].type == Begin) {
                 break;
             } else if (pos >= tokens.size()) {
-                cerr << tokens[pos].content << " except Begin";
+                cerr << tokens[pos].content << " except a Begin" << endl;
                 throw "missing Begin";
             }
         }
     }
     void parse_program_block() {
+        throw "Unimplemented";
         while (1) {
             // auto token = lexer.next_token();
         }
@@ -235,20 +254,21 @@ vector<Token> lex(string src) {
         tokens.push_back(token.value());
     }
 }
-int main() {
-    std::ifstream file("content.txt"); // 打开文件
-    if (!file) {
-        std::cerr << "文件无法打开！" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <filename>\n";
         return 1;
     }
+    auto filename = string(argv[1]);
 
-    // 使用istreambuf_iterator来读取整个文件内容
-    std::string content(std::istreambuf_iterator<char>(file), {});
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Could not open the file\n";
+        return 1;
+    }
+    string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
-    file.close();           // 关闭文件
-    string input = content; // from somewhere TODO
-
-    auto tokens = lex(input);
+    auto tokens = lex(content);
 
     auto pre_grammar = PreGrammar{tokens, 0};
     pre_grammar.parse_code();
